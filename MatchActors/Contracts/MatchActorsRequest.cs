8e1f43373374c9a0b811346f9a1cc52bc4b7bcdd
Blinov Vitaly <Blinov.V.A@mail.ru>
2022-07-06 00:52:41 +0400

    Выделил контракты

---
+++ MatchActors/Contracts/MatchActorsRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MatchActors.Contracts;
+
+public record MatchActorsRequest
+{
+    public string Actor1 { get; init; } = null!;
+    public string Actor2 { get; init; } = null!;
+    public bool MoviesOnly { get; init; }
+}
---
+++ MatchActors/Contracts/MatchActorsResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace MatchActors.Contracts;
+
+public record MatchActorsResponse
+{
+    public IEnumerable<string> Movies { get; init; } = null!;
+}
